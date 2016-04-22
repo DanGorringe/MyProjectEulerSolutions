820a14f03b698b8e1369331ce851ee8ecdf19dee
Dan Gorringe <DanGorringe@users.noreply.github.com>
2016-04-22 10:45:09 +0100

    Added files via upload

---
+++ Task10.c
@@ -0,0 +1,40 @@
+// This is task 10 of Project Euler, the sum of prime numbers under 2,000,000
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    long x;
+    long z;
+    long total;  // total of prime numbers
+    int primetest;
+    int dividetest;
+
+    x = 0;
+    z = 0;
+    total = 0;
+
+    // The loop goes while x < 2,000,000 and x increases by one every loop
+    for ( x = 2; x < 2000001; x++ ) {
+      dividetest = 0;
+      for ( z = 2; z < x ; z++ ) {
+        if ( !(x % z) ) {
+          dividetest = dividetest + 1;      // If can divide by any number below half the value of x
+          //x = x + 1;
+          //printf ("The current x is %ld and is divisble by ", x);
+          //printf (" %ld \n", z);
+        }
+      }
+      if ( dividetest == 0){
+        total = total + x;
+        printf ("The current prime x is %ld \n", x);
+      }
+    }
+      printf ("The prime total is %ld \n", total);                     // Then print the answer
+      return 0;
+}
+
+// Dan Gorringe April 2016
+// Finished
+// Answer = 142913828922
