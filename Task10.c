13e6185e8972d1771ad9ca2ce9f317e750815655
Dan Gorringe <dan.gorringe@hotmail.co.uk>
2017-07-13 11:00:11 +0100

    Testing github

--- Task10.c
+++ Task10.c
@@ -2,7 +2,7 @@
 
 #include <stdio.h>
 #include <math.h>
-#include <omp.h>
+// #include <omp.h>
 
 int main()
 {
@@ -19,9 +19,9 @@ int main()
     // The loop goes while x < 2,000,000 and x increases by one every loop
     for ( x = 2; x < 2000001; x++ ) {
       dividetest = 0;
-      #pragma omp parallel // This should run the for loop in parallel
+    //  #pragma omp parallel // This should run the for loop in parallel
       {
-      #pragma omp for
+      //#pragma omp for
       for ( z = 2; z < x ; z++ ) {
         if ( !(x % z) ) {
           dividetest = dividetest + 1;      // If can divide by any number below half the value of x
@@ -33,7 +33,7 @@ int main()
       }
       if ( dividetest == 0){
         total = total + x;
-        printf ("The current prime x is %ld \n", x);
+        //printf ("The current prime x is %ld \n", x);
       }
     }
       printf ("The prime total is %ld \n", total);                     // Then print the answer
