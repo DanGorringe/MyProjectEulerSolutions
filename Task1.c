19c432458c692eafc2abc84eb1099547bd818bbd
Dan Gorringe <dan.gorringe@hotmail.co.uk>
2018-01-26 11:05:18 +0000

    Corrected fizz buss

--- Task1.c
+++ Task1.c
@@ -1,28 +1,17 @@
-// This is task 1 of Project Euler, to find the sum of numbers under 1000 which are either divisible by 3 or 5
+// fizz Buzz
 
 #include <stdio.h>
 
 int main()
 {
-    int x;
-    int total;
-    total = 0;
-    // The loop goes while x < 10, and x increases by one every loop
-    for ( x = 0; x < 1000; x++ ) {
-      if ( !(x % 5) || !(x % 3) ) { // this will be executed if a is divisible by 5 and 3
-        printf ("%d \n", x);        // Print the current value of x which is either divisble by 3 or 5
-        printf ("Current total is %d \n", total);
-        total += x;
-      }
-    }
-    if ( x == 1000) {               // Once reached final value
-      printf ("Final total is %d \n", total);
-
-    }
-
-    getchar();
+  int x;
+  for (x = 0; x < 100; x++){
+    if (!(x % 3)) {printf("fizz ");}
+    else if (!(x % 5)) {printf("buzz ");}
+    else {printf("%d", x);}
+    printf(" \n");
+  }
+  return 0;
 }
 
-// Dan Gorringe March 2016
-// Completed
-// Answer = 233168
+// Dan Gorringe July 2017
